5ac4fcb83af901650fcbc11bff02533f1d2e7c6b
kylewstewart <kyle.w.stewart@mac.com>
2017-05-17 07:48:43 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-  // call functions here
+  preventSubmit()
 });
 
-// define functions here
+function preventSubmit() {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    console.log($('#item').val());
+    let new_item = $('<li></li>').addClass('task');
+    new_item.html($('#item').val());
+    new_item.appendTo('#list ol');
+  })
+}
